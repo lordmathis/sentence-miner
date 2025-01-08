914f0e7d5135ff1a1ae17222d0b4bfcce727eac5
LordMathis <matus@namesny.com>
2025-01-08 18:52:09 +0100

    initial route setup

--- miner/app.py
+++ miner/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from deck import create_deck
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/create", methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template("create.html")
+    
+    return create_deck()
+
+@app.route("/edit")
+def edit():
+    pass
+    
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 8000, debug=True)
---
+++ miner/deck.py
@@ -0,0 +1,2 @@
+def create_deck():
+    pass
