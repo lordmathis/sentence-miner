5c68fbb2756b3bb5ad737a00ed8327f83cf08867
LordMathis <matus@namesny.com>
2025-01-08 18:10:44 +0100

    Setup basic flask app

--- miner/app.py
+++ miner/app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 8000, debug=True)
